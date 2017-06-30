5d93bafd42faf212398f21fdf1f7b9b0e767108d
jondbo <jon@theobuchowskis.net>
2017-06-30 11:23:20 +0000

    enable editing Abouts and Events "key" attributes

--- models/Abouts.js
+++ models/Abouts.js
@@ -14,7 +14,8 @@ var Abouts = new keystone.List('Abouts', {
 Abouts.add({
   name: { type: String, required: true },
   images: { type: Types.CloudinaryImages, label: "Add these images here to use below." },
-  text: { type: Types.Html, wysiwyg: true, label: "HTML Text (To add an image, add images above, save and then copy the link of the image in the html text below.)" }
+  text: { type: Types.Html, wysiwyg: true, label: "HTML Text (To add an image, add images above, save and then copy the link of the image in the html text below.)" },
+  key: { type: Types.Key, initial: false, label: "URL key (must be unique)" }
 });
 
 Abouts.register();
