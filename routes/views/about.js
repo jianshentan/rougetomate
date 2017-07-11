61ad1966cd02b0d5b5698c49d80937ab5c4be311
Jon Obuchowski <jon@theobuchowskis.net>
2017-07-10 22:09:44 -0400

    refactor single About/Event query to handle invalid keys
    - switch to a more explicit query approach
    - return 404 status when query returns no results
    - handle any query errors

--- routes/views/about.js
+++ routes/views/about.js
@@ -10,7 +10,18 @@ exports = module.exports = function(req, res) {
   locals.section = 'about';
 
   // Lookup the selected item by key
-  view.query('about', keystone.list('Abouts').model.findOne({ key: req.params.key }));
+  view.on('init', function(next) {
+    var q = keystone.list('Abouts').model.findOne({ key: req.params.key });
+
+    q.exec(function(err, result) {
+      if (result != null) {
+        locals.about = result;
+      } else {
+        return res.status(404).send(keystone.wrapHTMLError('Sorry, no page could be found at this address (404)'));
+      }
+      next(err);
+    });
+  });
 
   // Render the view
   if ( req.isMobile ) {
