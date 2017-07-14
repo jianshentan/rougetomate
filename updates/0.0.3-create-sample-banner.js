17962be7a4030f52b9434fb0e304e7a92fa3c951
Jon Obuchowski <jon@theobuchowskis.net>
2017-07-14 13:19:21 -0400

    disable publishing on sample banner content

--- updates/0.0.3-create-sample-banner.js
+++ updates/0.0.3-create-sample-banner.js
@@ -4,9 +4,11 @@ exports.create = {
   Banners: [
     {
       name: 'RISE Up Dinner' ,
-      text: '<p>Rouge Tomate Chelsea is excited to host Change Food&rsquo;s RISE Up Dinner on July 24th. &nbsp;Click <a href="https://www.eventbrite.com/e/rise-dinner-at-rouge-tomate-a-benefit-for-change-food-tickets-34759081348">here</a>&nbsp;to learn more and get your tickets!</p>' ,
+      text: '<p>Rouge Tomate Chelsea is excited to host Change Food&rsquo;s RISE Up Dinner on July 24th. &nbsp;Click <a href="https://www.eventbrite.com/e/rise-dinner-at-rouge-tomate-a-benefit-for-change-food-tickets-34759081348">here</a>&nbsp;to learn more and get your tickets!</p>'
+      /* ,
       displayStartDate: new Date(Date.parse('2017/7/1 12:00:00 AM')) ,
       displayEndDate: new Date(Date.parse('2017/7/25 12:00:00 AM')) ,
+      */
     } ,
     {
       name: 'Boozy Brunch' ,
